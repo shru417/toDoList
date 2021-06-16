fb13ef33cdc515e8e3148992b2d0c560356e0ede
shru417 <shruti.kumari6376@gmail.com>
2021-06-16 13:06:16 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const inp = document.getElementById('inp');
+const btn = document.getElementById('addTodo');
+const list = document.getElementById('list');
+btn.onclick=function (e) {
+
+    const todoText = inp.nodeValue;
+    const li = document.createElement('li');
+    li.innerText = todoText;
+
+    li.onclick=function  (e) {
+        console.log(e.target);
+        e.target.remove();
+    }
+
+    list.append(li);
+    inp.value ="";
+}
